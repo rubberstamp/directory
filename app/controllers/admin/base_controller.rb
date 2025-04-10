dc20bc97ff06d9a4f9a63a07fa7de5074ae038d8
James Kennedy <james.kennedy@rubberstamp.io>
2025-04-10 08:50:26 +0100

    Fix RuboCop linting errors in controllers

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,11 @@
 class Admin::BaseController < ApplicationController
   before_action :authenticate_user!
   before_action :check_admin
-  
-  layout 'admin'
-  
+
+  layout "admin"
+
   private
-  
+
   def check_admin
     unless current_user.admin?
       flash[:alert] = "You must be an admin to access this section"
